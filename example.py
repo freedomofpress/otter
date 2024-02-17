f7ae14d98b66cdc8b09bf2ff94770de91b6d0ea2
giulio <66009328+lsd-cat@users.noreply.github.com>
2024-02-17 18:03:59 +0100

    Working Qubes automation example

--- example.py
+++ example.py
@@ -1,13 +1,32 @@
+from configparser import ConfigParser
 import vmware
 import logging
 from otter import Otter
+from helpers import qubes
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-adapter = vmware.vmwareAdapter("root", "pass", "ip", vmname="Router", headers={}, verify=False)
+config = ConfigParser()
+config.read('otter.ini')
+host = config.get('esx', 'server')
+username = config.get('esx', 'username')
+password = config.get('esx', 'password')
+vms = config.get('esx', 'vms')
 
+adapter = vmware.vmwareAdapter(username, password, host, headers={}, verify=False)
 
-vm = adapter.getFreeMachine("name*")
-otter = Otter(vm, adapter, testfile=None, screenrecord=None, outputfolder="/tmp/test1")
+# Look for a free machine with "Qubes 42 Otter" name prefix
+vm = adapter.getFreeMachine(vms)
+# Initialize otter, will spawn VNC + serial
+otter = Otter(vm, adapter, testfile=None, screenrecord=None)
+# Serial console login example, with success checking
+qubes.login_serial(otter)
+# VNC login example, with success checking
+qubes.login_gui(otter)
+# Run a command on any qube via serial
+qubes.run_command_in_qube_serial_and_wait(otter, "sys-net", "id", "groups")
+# Example of starting a GUI app via serial
+qubes.launch_terminal_dom0(otter)
+# Save logs and poweroff
 otter.exit()
